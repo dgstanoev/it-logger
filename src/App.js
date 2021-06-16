3bc43ec06556e8e9bdf61c802013ae12e8b9d591
dgstanoev <dim.stanoev94@gmail.com>
2021-06-16 16:28:10 +0300

    Add addLogModal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import { Fragment, useEffect } from 'react';
+import AddBtn from './components/layout/AddBtn';
 import SearchBar from './components/layout/SearchBar';
+import AddLogModal from './components/logs/AddLogModal';
 import Logs from './components/logs/Logs';
 
 const App = () => {
@@ -12,8 +14,10 @@ const App = () => {
   return (
     <Fragment>
       <SearchBar />
-      <div className="container">
-        <Logs/>
+      <div className='container'>
+        <AddBtn />
+        <AddLogModal />
+        <Logs />
       </div>
     </Fragment>
   );
